3e0f3eefcc419f53daf5d0dab1d5baa01ce486cf
Christian Merkl <knueppl@gmx.de>
2019-04-14 10:33:18 +0200

    Tied to include libfrancor.

---
+++ src/line_detector_node.cpp
@@ -0,0 +1,9 @@
+#include <francor_processing/data_processing_pipeline.h>
+#include <francor_processing/data_processing_pipeline_stage.h>
+
+int main(int argc, char** argv)
+{
+  francor::processing::DataProcssingPipeline pipeline;
+
+  return 0;
+}
